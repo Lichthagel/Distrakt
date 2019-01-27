c4ac8dac2dd052d71cbd5539a306af0a01d7fe02
Vngdv <andre.rica@icloud.com>
2019-01-27 13:16:37 +0100

    Added success message to the subscribe command

--- src/commands/subscribe.rs
+++ src/commands/subscribe.rs
@@ -1,15 +1,13 @@
+use crate::{
+    schema::users::dsl::discord_id, schema::users::dsl::subscribed, schema::users::dsl::users,
+    wrappers::Sqlite,
+};
+use diesel::{prelude::*, query_dsl::RunQueryDsl};
 use serenity::{
     framework::{standard::Args, standard::Command, standard::CommandError},
     model::channel::Message,
     prelude::Context,
 };
-use crate::{
-    wrappers::{Sqlite},
-    schema::users::dsl::users,
-    schema::users::dsl::subscribed,
-    schema::users::dsl::discord_id
-};
-use diesel::{prelude::*, query_dsl::RunQueryDsl};
 
 pub struct Subscribe;
 
@@ -21,10 +19,23 @@ impl Command for Subscribe {
             .ok_or("Couldn't extract connection".to_owned())
             .and_then(|conn| conn.lock().map_err(|e| e.to_string()))
             .and_then(|conn| {
-                let target = users.filter(discord_id.eq(msg.author.id.0 as i64));
-                diesel::update(target).set(subscribed.eq(true)).execute(&*conn)
+                diesel::update(users.filter(discord_id.eq(msg.author.id.0 as i64)))
+                    .set(subscribed.eq(true))
+                    .execute(&*conn)
                     .map_err(|e| e.to_string())
-            }).and_then(|_| Ok(()))
+            })
+            .and_then(|_| {
+                msg.author
+                    .direct_message(&ctx, |m| {
+                        m.embed(|e| {
+                            e.title("Success")
+                                .description("You have successfully subscribed!")
+                                .color((237u8, 28u8, 36u8))
+                        })
+                    })
+                    .map_err(|e| e.to_string())
+            })
+            .and_then(|_| Ok(()))
             .map_err(|e| {
                 println!("{}", e);
                 msg.author
