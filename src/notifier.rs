3374902894ceeaebfd0093f14af40ebd3536b2fa
Jens Gatzweiler <lichthagel@tuta.io>
2019-01-28 11:04:02 +0100

    Improved sync thread a bit

--- src/notifier.rs
+++ src/notifier.rs
@@ -3,44 +3,40 @@ use crate::{
     wrappers::Sqlite,
 };
 use diesel::{query_dsl::*, NullableExpressionMethods};
+use futures::future::Future;
 use futures::future::IntoFuture;
-use serenity::prelude::Context;
 
-pub fn sync_thread(ctx: Context) {
-    let fut = ctx
-        .data
-        .read()
-        .get::<Sqlite>()
-        .ok_or(())
-        .and_then(|conn| conn.lock().map_err(|_| ()))
-        .and_then(|conn| {
-            notify::dsl::notify
-                .left_join(users::dsl::users)
-                .select((
-                    notify::channel,
-                    notify::type_,
-                    notify::data,
-                    users::access_token.nullable(),
-                ))
-                .load::<(i64, i32, Option<i64>, Option<String>)>(&*conn)
-                .map_err(|_| ())
-        })
-        .map(|res: Vec<(i64, i32, Option<i64>, Option<String>)>| {
-            res.into_iter()
-                .map(|(channel, type_, data, access_token)| {
-                    (
-                        channel as u64,
-                        type_ as u8,
-                        data.map(|i| i as u64),
-                        access_token,
-                    )
-                })
-                .collect()
-        })
-        .and_then(|d: Vec<(u64, u8, Option<u64>, Option<String>)>| {
-            dbg!(d);
-            Ok(())
-        });
+pub fn sync_thread(conn: &Sqlite) {
+    let fut = match conn.lock() {
+        Ok(conn) => notify::dsl::notify
+            .left_join(users::dsl::users)
+            .select((
+                notify::channel,
+                notify::type_,
+                notify::data,
+                users::access_token.nullable(),
+            ))
+            .load::<(i64, i32, Option<i64>, Option<String>)>(&*conn)
+            .map(|res: Vec<(i64, i32, Option<i64>, Option<String>)>| {
+                res.into_iter()
+                    .map(|(channel, type_, data, access_token)| {
+                        (
+                            channel as u64,
+                            type_ as u8,
+                            data.map(|i| i as u64),
+                            access_token,
+                        )
+                    })
+                    .collect()
+            })
+            .map_err(|_| ()),
+        Err(_) => Err(()),
+    }
+    .into_future()
+    .and_then(|d: Vec<(u64, u8, Option<u64>, Option<String>)>| {
+        dbg!(d);
+        Ok(())
+    });
 
     tokio::run(fut.into_future());
 }
